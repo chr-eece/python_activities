947bbb6f80e4d47c30e5623ba4a6625b5a25d97b
Christopher Macharia Mwangi <150504655+chr-eece@users.noreply.github.com>
2024-09-15 22:59:39 +0300

    Add files via upload

---
+++ exe1.py
@@ -0,0 +1,6 @@
+#program for surface area of a cylinder
+r=int(input("Enter the radius"))
+h=int(input("Enter the height"))
+sa=2*3.142*r*r +2*3.142*r*h
+print("The Surface Area is", sa)
+print(type(sa))
